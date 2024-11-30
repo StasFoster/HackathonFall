9869d28d9458a0fb670e2c7bd9c340944bd2d771
LAA <stardog2005.24@gmail.com>
2024-11-30 18:53:55 +0400

    сохр

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hackathon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Hackathon.Controllers
 {
     public class TestController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Save(Test model)
+        {
+            // Получение ответов
+            var answers = model.Answers;
+
+            // Запись ответов в файл
+            using (var file = new StreamWriter("contacts.txt", true))
+            {
+                file.WriteLine(string.Join("|", answers));
+            }
+
+            return View("Perfect", model); // Изменено для возврата представления Perfect с моделью Test
+        }
     }
 }
+
+
+
---
+++ Models/Test.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Hackathon.Models
+{
+    public class Test
+    {
+        public List<string> Answers { get; set; } = new List<string>();
+    }
+}
