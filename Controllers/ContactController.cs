56ba89307b680337348dcf5e14a271ec4595018d
StasFoster <111638493+StasFoster@users.noreply.github.com>
2024-11-30 10:21:48 +0400

    Добавьте файлы проекта.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,48 @@
+﻿using Hackathon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Hackathon.Controllers
+{
+    public class ContactController : Controller
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                contacts.Add(contact);
+
+                // Запись списка контактов в файл
+                using (StreamWriter sw = new StreamWriter("contacts.txt", true))
+                {
+                    foreach (var c in contacts)
+                    {
+                        sw.WriteLine($"{c.Name},{c.SurName},{c.Login},{c.Password}");
+                    }
+                }
+
+                return RedirectToAction("Good");
+            }
+
+            return View("Index");
+        }
+
+        public IActionResult Good()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(contacts);
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hackathon.Models
+{
+    public class Contact
+    {
+        [Display(Name = " ")]
+        public string Name { get; set; }
+
+        [Display(Name = " ")]
+        public string SurName { get; set; }
+
+        [Display(Name = " ")]
+        public string Login { get; set; }
+
+        [Display(Name = " ")]
+        public string Password { get; set; }
+    }
+
+}
+
